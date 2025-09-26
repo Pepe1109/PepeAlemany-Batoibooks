92652ba7617fdaa239dfbdef09236be0e9cf50ac
Pepe1109 <palemanyfct@gmail.com>
2025-09-26 19:44:47 +0200

    Estructura mejorada

--- src/functions.js
+++ src/functions.js
@@ -9,3 +9,76 @@
     if (index === -1) throw new Error('Book not found');
     return index;
  }
+
+
+function bookExists(books, userId, moduleCode) {
+    
+}
+
+function booksFromUser(books, userId) {
+    
+}
+
+function booksFromModule(books, moduleCode) {
+    
+}
+
+function booksCheeperThan(books, price) {
+    
+}
+
+function booksWithStatus(books, status) {
+    
+}
+
+function averagePriceOfBooks(books) {
+    
+}
+
+function booksOfTypeNotes(books) {
+    
+}
+
+function booksNotSold(books) {
+    
+}
+
+function incrementPriceOfbooks(books, percentage) {
+    
+}
+
+
+function getUserById(users, userId) {
+    
+}
+
+function getUserIndexById(users, userId) {
+    
+}
+
+function getUserByNickName(users, nick) {
+    
+}
+
+
+function getModuleByCode(modules, moduleCode) {
+    
+}
+
+ export {
+  getBookById,
+  getBookIndexById,
+  bookExists,
+  booksFromUser,
+  booksFromModule,
+  booksCheeperThan,
+  booksWithStatus,
+  averagePriceOfBooks,
+  booksOfTypeNotes,
+  booksNotSold,
+  incrementPriceOfbooks,
+  getUserById,
+  getUserIndexById,
+  getUserByNickName,
+  getModuleByCode
+}
