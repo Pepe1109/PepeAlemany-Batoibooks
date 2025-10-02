7ec9d34074928cb29ce950f9196e83935d9354d6
Pepe1109 <palemanyfct@gmail.com>
2025-10-02 11:21:56 +0200

    Práctica terminada, funciones y main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,29 @@
 import data from  './services/datos.js';
+import {
+  getBookById,
+  getBookIndexById,
+  bookExists,
+  booksFromUser,
+  booksFromModule,
+  booksCheeperThan,
+  booksWithStatus,
+  averagePriceOfBooks,
+  booksOfTypeNotes,
+  booksNotSold,
+  incrementPriceOfbooks,
+  getUserById,
+  getUserIndexById,
+  getUserByNickName,
+  getModuleByCode
+} from './functions.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <center>Abre la terminal</center>
+    <center><img src="./public/logoBatoi.png"></center>
+    <center>Abre la terminal</center
   </div>
 `
+
+console.log(booksFromModule(data.books, "5021"))
+console.log(booksWithStatus(data.books, "new"))
+console.log(incrementPriceOfbooks(data.books, 0.1))
